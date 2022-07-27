59c335b9863dade36820397a268c7ef22a0fc200
bpoon-selina <betty.poon@selinafinance.co.uk>
2022-07-27 11:35:16 +0100

    LA-12: Merge changes from master and resolve conflicts

--- src/main/java/com/selina/lending/internal/repository/MiddlewareRepository.java
+++ src/main/java/com/selina/lending/internal/repository/MiddlewareRepository.java
@@ -18,7 +18,6 @@
 package com.selina.lending.internal.repository;
 
 import com.selina.lending.internal.service.application.domain.ApplicationResponse;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 public interface MiddlewareRepository {
     ApplicationResponse getApplicationById(String id);
