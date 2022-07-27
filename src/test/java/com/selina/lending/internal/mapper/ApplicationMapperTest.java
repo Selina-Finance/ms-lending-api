f3b87a496ba234095d114d5ce88dfd2318ad497d
bpoon-selina <betty.poon@selinafinance.co.uk>
2022-07-27 11:25:10 +0100

    LA-12: Add unit tests

---
+++ src/test/java/com/selina/lending/internal/mapper/ApplicationMapperTest.java
@@ -0,0 +1,81 @@
+/*
+ *   Copyright 2022 Selina Finance
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License");
+ *      you may not use this file except in compliance with the License.
+ *      You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing, software
+ *      distributed under the License is distributed on an "AS IS" BASIS,
+ *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *      See the License for the specific language governing permissions and
+ *      limitations under the License.
+ *
+ */
+
+package com.selina.lending.internal.mapper;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+
+import com.selina.lending.internal.dto.DIPApplicationDto;
+import com.selina.lending.internal.service.application.domain.Application;
+
+public class ApplicationMapperTest extends MapperBase {
+
+    @Test
+    public void mapToDIPApplicationDto() {
+        //Given
+        Application application = getApplication();
+
+        //When
+        DIPApplicationDto applicationDto = ApplicationMapper.INSTANCE.mapToDIPApplicationDto(application);
+
+        //Then
+        assertThat(applicationDto.getId(), equalTo(APPLICATION_ID));
+        assertThat(applicationDto.getApplicants().size(), equalTo(1));
+        assertThat(applicationDto.getApplicants().get(0).getTitle(), equalTo(TITLE));
+        assertThat(applicationDto.getApplicants().get(0).getFirstName(), equalTo(FIRST_NAME));
+        assertThat(applicationDto.getApplicants().get(0).getLastName(), equalTo(LAST_NAME));
+        assertThat(applicationDto.getRequestType(), equalTo(DIP_APPLICATION_TYPE));
+        assertThat(applicationDto.getLoanInformation().getLoanPurpose(), equalTo(LOAN_PURPOSE));
+        assertThat(applicationDto.getLoanInformation().getRequestedLoanAmount(), equalTo(LOAN_AMOUNT));
+        assertThat(applicationDto.getLoanInformation().getFacilities().size(), equalTo(1));
+        assertThat(applicationDto.getLoanInformation().getFacilities().get(0).getAllocationAmount(), equalTo(ALLOCATION_AMOUNT));
+        assertThat(applicationDto.getLoanInformation().getFacilities().get(0).getAllocationPurpose(), equalTo(ALLOCATION_PURPOSE));
+        assertThat(applicationDto.getPropertyDetails().getAddressLine1(), equalTo(ADDRESS_LINE_1));
+        assertThat(applicationDto.getPropertyDetails().getAddressLine2(), equalTo(ADDRESS_LINE_2));
+        assertThat(applicationDto.getOffers().get(0).getActive(), equalTo(true));
+        assertThat(applicationDto.getOffers().get(0).getId(), equalTo(OFFER_ID));
+        assertThat(applicationDto.getOffers().get(0).getProductCode(), equalTo(PRODUCT_CODE));
+    }
+
+    @Test
+    public void mapToApplication() {
+        //Given
+        DIPApplicationDto applicationDto = getDIPApplicationDto();
+
+        //When
+        Application application = ApplicationMapper.INSTANCE.mapToApplication(applicationDto);
+
+        //Then
+        assertThat(application.getId(), equalTo(APPLICATION_ID));
+        assertThat(application.getApplicants().size(), equalTo(1));
+        assertThat(application.getApplicants().get(0).getTitle(), equalTo(TITLE));
+        assertThat(application.getApplicants().get(0).getFirstName(), equalTo(FIRST_NAME));
+        assertThat(application.getApplicants().get(0).getLastName(), equalTo(LAST_NAME));
+        assertThat(application.getApplicationType(), equalTo(DIP_APPLICATION_TYPE));
+        assertThat(application.getLoanInformation().getLoanPurpose(), equalTo(LOAN_PURPOSE));
+        assertThat(application.getLoanInformation().getRequestedLoanAmount(), equalTo(LOAN_AMOUNT));
+        assertThat(application.getLoanInformation().getFacilities().size(), equalTo(1));
+        assertThat(application.getPropertyDetails().getAddressLine1(), equalTo(ADDRESS_LINE_1));
+        assertThat(application.getPropertyDetails().getAddressLine2(), equalTo(ADDRESS_LINE_2));
+        assertThat(application.getOffers().get(0).getActive(), equalTo(true));
+        assertThat(application.getOffers().get(0).getId(), equalTo(OFFER_ID));
+        assertThat(application.getOffers().get(0).getProductCode(), equalTo(PRODUCT_CODE));
+    }
+}
---
+++ src/test/java/com/selina/lending/internal/mapper/PropertyDetailsMapperTest.java
@@ -0,0 +1,118 @@
+/*
+ *   Copyright 2022 Selina Finance
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License");
+ *      you may not use this file except in compliance with the License.
+ *      You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing, software
+ *      distributed under the License is distributed on an "AS IS" BASIS,
+ *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *      See the License for the specific language governing permissions and
+ *      limitations under the License.
+ *
+ */
+
+package com.selina.lending.internal.mapper;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.Test;
+
+import com.selina.lending.internal.dto.DIPPropertyDetailsDto;
+import com.selina.lending.internal.dto.PropertyDetailsDto;
+import com.selina.lending.internal.service.application.domain.PropertyDetails;
+
+public class PropertyDetailsMapperTest extends MapperBase {
+
+    @Test
+    public void mapToPropertyDetailsDto() {
+        //Given
+        PropertyDetails propertyDetails = getPropertyDetails();
+
+        //When
+        PropertyDetailsDto propertyDetailsDto = PropertyDetailsMapper.INSTANCE.mapToPropertyDetailsDto(propertyDetails);
+
+        //Then
+        assertThat(propertyDetailsDto.getAddressLine1(), equalTo(ADDRESS_LINE_1));
+        assertThat(propertyDetailsDto.getAddressLine2(), equalTo(ADDRESS_LINE_2));
+        assertThat(propertyDetailsDto.getPurchaseValue(), equalTo(PURCHASE_VALUE));
+        assertThat(propertyDetailsDto.getCountry(), equalTo(COUNTRY));
+        assertThat(propertyDetailsDto.getBuildingNumber(), equalTo(BUILDING_NUMBER));
+        assertThat(propertyDetailsDto.getBuildingName(), equalTo(BUILDING_NAME));
+        assertThat(propertyDetailsDto.getCity(), equalTo(CITY));
+        assertThat(propertyDetailsDto.getPostcode(), equalTo(POSTCODE));
+        assertThat(propertyDetailsDto.getCounty(), equalTo(COUNTY));
+        assertThat(propertyDetailsDto.getWhenLastPurchased(), equalTo(WHEN_LAST_PURCHASED));
+    }
+    @Test
+    public void mapToDIPPropertyDetailsDto() {
+        //Given
+        PropertyDetails propertyDetails = getPropertyDetails();
+
+        //When
+        DIPPropertyDetailsDto dipPropertyDetailsDto = PropertyDetailsMapper.INSTANCE.mapToPropertyDetailsDto(propertyDetails);
+
+        //Then
+        assertThat(dipPropertyDetailsDto.getAddressLine1(), equalTo(ADDRESS_LINE_1));
+        assertThat(dipPropertyDetailsDto.getAddressLine2(), equalTo(ADDRESS_LINE_2));
+        assertThat(dipPropertyDetailsDto.getPurchaseValue(), equalTo(PURCHASE_VALUE));
+        assertThat(dipPropertyDetailsDto.getCountry(), equalTo(COUNTRY));
+        assertThat(dipPropertyDetailsDto.getBuildingNumber(), equalTo(BUILDING_NUMBER));
+        assertThat(dipPropertyDetailsDto.getBuildingName(), equalTo(BUILDING_NAME));
+        assertThat(dipPropertyDetailsDto.getCity(), equalTo(CITY));
+        assertThat(dipPropertyDetailsDto.getPostcode(), equalTo(POSTCODE));
+        assertThat(dipPropertyDetailsDto.getCounty(), equalTo(COUNTY));
+        assertThat(dipPropertyDetailsDto.getWhenLastPurchased(), equalTo(WHEN_LAST_PURCHASED));
+        assertThat(dipPropertyDetailsDto.getPropertyType(), equalTo(PROPERTY_TYPE));
+        assertThat(dipPropertyDetailsDto.getNumberOfBedrooms(), equalTo(NUMBER_OF_BEDROOMS));
+    }
+
+    @Test
+    public void mapToPropertyDetailsFromPropertyDetailsDto() {
+        //Given
+        PropertyDetailsDto propertyDetailsDto = getPropertyDetailsDto();
+
+        //When
+        PropertyDetails propertyDetails = PropertyDetailsMapper.INSTANCE.mapToPropertyDetails(propertyDetailsDto);
+
+        //Then
+        assertThat(propertyDetails.getAddressLine1(), equalTo(ADDRESS_LINE_1));
+        assertThat(propertyDetails.getAddressLine2(), equalTo(ADDRESS_LINE_2));
+        assertThat(propertyDetails.getPurchaseValue(), equalTo(PURCHASE_VALUE));
+        assertThat(propertyDetails.getCountry(), equalTo(COUNTRY));
+        assertThat(propertyDetails.getBuildingNumber(), equalTo(BUILDING_NUMBER));
+        assertThat(propertyDetails.getBuildingName(), equalTo(BUILDING_NAME));
+        assertThat(propertyDetails.getCity(), equalTo(CITY));
+        assertThat(propertyDetails.getPostcode(), equalTo(POSTCODE));
+        assertThat(propertyDetails.getCounty(), equalTo(COUNTY));
+        assertNull(propertyDetails.getHasAGarage());
+    }
+
+    @Test
+    public void mapToPropertyDetailsFromDIPPropertyDetailsDto() {
+        //Given
+        DIPPropertyDetailsDto dipPropertyDetailsDto = getDIPPropertyDetailsDto();
+
+        //When
+        PropertyDetails propertyDetails = PropertyDetailsMapper.INSTANCE.mapToPropertyDetails(dipPropertyDetailsDto);
+
+        //Then
+        assertThat(propertyDetails.getAddressLine1(), equalTo(ADDRESS_LINE_1));
+        assertThat(propertyDetails.getAddressLine2(), equalTo(ADDRESS_LINE_2));
+        assertThat(propertyDetails.getPurchaseValue(), equalTo(PURCHASE_VALUE));
+        assertThat(propertyDetails.getCountry(), equalTo(COUNTRY));
+        assertThat(propertyDetails.getBuildingNumber(), equalTo(BUILDING_NUMBER));
+        assertThat(propertyDetails.getBuildingName(), equalTo(BUILDING_NAME));
+        assertThat(propertyDetails.getCity(), equalTo(CITY));
+        assertThat(propertyDetails.getPostcode(), equalTo(POSTCODE));
+        assertThat(propertyDetails.getCounty(), equalTo(COUNTY));
+        assertThat(propertyDetails.getPropertyType(), equalTo(PROPERTY_TYPE));
+        assertThat(propertyDetails.getNumberOfBedrooms(), equalTo(NUMBER_OF_BEDROOMS));
+        assertThat(propertyDetails.getHasAGarage(), equalTo(true));
+    }
+}
