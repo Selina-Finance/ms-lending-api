992a747a418e15528738ee180b159b30ed57933a
Ângelo Paiva <angelo.paiva@present-technologies.com>
2023-05-15 15:38:40 +0100

    test(GRO-1987): Add class to help with Products

---
+++ src/test/java/com/selina/lending/testHelper/ProductHelper.java
@@ -0,0 +1,13 @@
+package com.selina.lending.testHelper;
+
+import java.util.Random;
+
+public class ProductHelper {
+    public static final String[] PRODUCT_CODES = {"Fix0005", "Fix0006", "Fix0007", "Fix011", "Fix013", "Var0005",
+            "Var0006", "Var0007", "Var0008", "Var0009", "Fix008", "Fix009", "Fix010", "Fix012", "Fix014"};
+
+    public static String getRandomProductCode() {
+        int random = new Random().nextInt(PRODUCT_CODES.length);
+        return PRODUCT_CODES[random];
+    }
+}
