10e2642cfa7a47861753c22c4020767c6f04a6bf
Roman Lopariev <roman.lopariev@selinafinance.co.uk>
2023-07-12 01:10:47 +0200

    delete excess KafkaSendEventException

--- src/main/java/com/selina/lending/internal/exception/KafkaSendEventException.java
+++
@@ -1,8 +0,0 @@
-package com.selina.lending.internal.exception;
-
-public class KafkaSendEventException extends RuntimeException {
-
-    public KafkaSendEventException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
