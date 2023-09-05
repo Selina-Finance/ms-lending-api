73b2eab0faf0b800bf32ef5947544bfc0930b9cf
Roman Lopariev <roman.lopariev@selinafinance.co.uk>
2023-09-05 09:27:38 +0200

    Revert "test(GRO-2442): Update tests"
    This reverts commit b8adf5383cb17273b69b9b1e28c39fe64c030503.

--- src/test/java/com/selina/lending/api/mapper/qq/selection/QuickQuoteApplicationRequestMapperTest.java
+++ src/test/java/com/selina/lending/api/mapper/qq/selection/QuickQuoteApplicationRequestMapperTest.java
@@ -19,16 +19,13 @@ package com.selina.lending.api.mapper.qq.selection;
 
 import com.selina.lending.api.mapper.MapperBase;
 import com.selina.lending.httpclient.selection.dto.request.Application;
-import com.selina.lending.httpclient.selection.dto.request.Expenditure;
 import com.selina.lending.httpclient.selection.dto.request.FilterQuickQuoteApplicationRequest;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 
@@ -56,8 +53,8 @@ class QuickQuoteApplicationRequestMapperTest extends MapperBase {
         assertThat(applicationRequest.getApplication().getFees(), nullValue());
 
         assertPropertyDetails(application);
+
         assertPriorCharges(applicationRequest);
-        assertExpenditures(application.getExpenditures());
     }
 
     @Test
@@ -120,12 +117,5 @@ class QuickQuoteApplicationRequestMapperTest extends MapperBase {
         assertThat(fees.getIsAddIntermediaryFeeToLoan(), equalTo(false));
         assertThat(fees.getIsAddArrangementFeeSelinaToLoan(), equalTo(true));
         assertThat(fees.getArrangementFee(), equalTo(ARRANGEMENT_FEE));
-    }
-
-    private void assertExpenditures(List<Expenditure> expenditures) {
-        assertThat(expenditures, hasSize(1));
-
-        var expenditure = expenditures.get(0);
-        assertThat(expenditure.getExpenditureType(), equalTo(EXPENDITURE_TYPE));
     }
 }
