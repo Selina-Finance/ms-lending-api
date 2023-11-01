bbeb1da4eae3226449dd0f9f8ff68ce8dd1d29ea
Roman Lopariev <roman.lopariev@selinafinance.co.uk>
2023-11-01 03:50:21 +0100

    fix(GRO-2692): fix eligibility property infor deserialization

--- src/main/java/com/selina/lending/httpclient/eligibility/dto/response/PropertyInfo.java
+++ src/main/java/com/selina/lending/httpclient/eligibility/dto/response/PropertyInfo.java
@@ -2,9 +2,11 @@ package com.selina.lending.httpclient.eligibility.dto.response;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
 
 @Builder
 @Data
+@Jacksonized
 public class PropertyInfo {
 
     private Double estimatedValue;
