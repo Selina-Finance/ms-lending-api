419a4811b0f3bf7502abf6ce08781cd890ebe5a1
Roman Lopariev <roman.lopariev@selinafinance.co.uk>
2024-03-07 02:30:38 +0100

    chore(GRO-2936): A/B pre-approvals test

---
+++ src/main/java/com/selina/lending/util/ABTestUtils.java
@@ -0,0 +1,35 @@
+package com.selina.lending.util;
+
+import com.selina.lending.api.dto.qq.request.QuickQuoteApplicantDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface ABTestUtils {
+
+    Logger log = LoggerFactory.getLogger(ABTestUtils.class);
+
+    static boolean hasOddPrimaryApplicantBirthday(List<QuickQuoteApplicantDto> applicants) {
+        try {
+            var primaryApplicant = findPrimaryApplicant(applicants);
+            return primaryApplicant.isPresent() && hasOddApplicantBirthday(primaryApplicant.get());
+        } catch (Exception ex) {
+            log.warn("Error checking if primary applicant birthday is odd! Return false!", ex);
+            return false;
+        }
+    }
+
+    private static Optional<QuickQuoteApplicantDto> findPrimaryApplicant(List<QuickQuoteApplicantDto> applicants) {
+        return applicants.stream()
+                .filter(applicant -> applicant.getPrimaryApplicant() != null && applicant.getPrimaryApplicant())
+                .findFirst();
+    }
+
+    private static boolean hasOddApplicantBirthday(QuickQuoteApplicantDto applicant) {
+        var birthday = LocalDate.parse(applicant.getDateOfBirth());
+        return birthday.getDayOfMonth() % 2 != 0;
+    }
+}
