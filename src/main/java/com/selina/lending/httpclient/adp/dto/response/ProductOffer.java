37cd0871c22cdffaec810b4a910114ddd12a6d83
bpoon-selina <betty.poon@selinafinance.co.uk>
2024-04-09 16:55:44 +0100

    Revert "AM-180 - Remove unused fields in adp and mw dto"
    This reverts commit 79125ff931c7feeb599788005e15403b9838fbb6.

--- src/main/java/com/selina/lending/httpclient/adp/dto/response/ProductOffer.java
+++ src/main/java/com/selina/lending/httpclient/adp/dto/response/ProductOffer.java
@@ -67,7 +67,10 @@ public class ProductOffer {
     Double initialMargin;
     Double poundPaidPerBorrowed;
     Double monthlyPaymentStressed;
+    String decisionAml;
+    String decisionFraud;
     Double baseRateStressed;
+    Double aprcStressed;
     Double initialRateMinimum;
     Double reversionMargin;
     Double reversionRateMinimum;
